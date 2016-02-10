04d73262283e4ecc0432b0faacb0d52baf258356
franckPicardat <franck-ten@hotmail.fr>
2016-02-10 17:07:10 +0100

    exercice 2 question 3

---
+++ src/main/java/dcll/fpic/App.java
@@ -0,0 +1,13 @@
+package dcll.fpic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
